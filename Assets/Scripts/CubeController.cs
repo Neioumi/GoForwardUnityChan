eb903bee5b4b62afb017bc439757207979cbaac3
Neioumi <neioumi@me.com>
2017-07-13 00:17:56 +0900

    キューブの効果音を追加

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -7,10 +7,13 @@ public class CubeController : MonoBehaviour {
 	private float speed = -0.2f;
 	// 消滅位置
 	private float deadLine = -10;
+	// 効果音用のコンポーネントを入れる
+	private AudioSource cubeSound;
 
 	// Use this for initialization
 	void Start () {
-		
+		// AudioSourceのコンポーネントを取得
+		cubeSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +23,18 @@ public class CubeController : MonoBehaviour {
 		// 画面外に出たら破棄する
 		if (transform.position.x < this.deadLine){
 			Destroy(gameObject);
+		}
+	}
+
+	// キューブが接触した時
+	void OnCollisionEnter2D(Collision2D other) {
+		// キューブが、キューブまたは地面と接触した時（タグで判定）
+		if (other.gameObject.tag == "Cube" || other.gameObject.tag == "Ground") {
+			Debug.Log("キューブまたは地面と接触");
+			// 音声ファイルを再生
+			cubeSound.PlayOneShot(cubeSound.clip);
+		} else if (other.gameObject.tag == "UnityChan") {
+			Debug.Log("Unityちゃんと接触");
 		}
 	}
 }
