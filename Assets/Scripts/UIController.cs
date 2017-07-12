a34a552190863dbc531c6800d0646ad81f0b1de2
Neioumi <neioumi@me.com>
2017-07-13 01:45:43 +0900

    PlayerPrefsで最高記録を表示できるようにしてみた

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -9,18 +9,30 @@ public class UIController : MonoBehaviour {
 	private GameObject gameOverText;
 	// 走行距離テキスト
 	private GameObject runLengthText;
+	// 最高記録テキスト
+	private GameObject bestRecordText;
 	// 走行距離
 	private float len = 0;
+	// 最高記録
+	private float bestRecord = 0;
 	// 走る速度
 	private float speed = 0.03f;
 	// ゲームオーバー判定
 	private bool isGameOver = false;
+	// ハイスコア保存用キー
+	private string key = "BestRecord";
 
 	// Use this for initialization
 	void Start () {
 		// シーンビューからオブジェクトの実体を検索
 		this.gameOverText = GameObject.Find("GameOver");
 		this.runLengthText = GameObject.Find("RunLength");
+		this.bestRecordText = GameObject.Find("RecordText");
+
+		// 最高記録の値を、PlayerPrefsから読み出し。nullだったら0を返す
+		this.bestRecord = PlayerPrefs.GetFloat(key, 0);
+		// 最高記録を表示
+		this.bestRecordText.GetComponent<Text>().text = "Best Record: " + bestRecord.ToString("F2") + "m";
 	}
 	
 	// Update is called once per frame
@@ -33,8 +45,21 @@ public class UIController : MonoBehaviour {
 			this.runLengthText.GetComponent<Text>().text = "Distance: " + len.ToString("F2") + "m";
 		}
 
-		// ゲームオーバーの時
+		// 走行距離が最高記録を超えていたら
+		if (len > bestRecord) {
+			Debug.Log("最高記録更新！");
+			Debug.Log("bestRecord: "+ bestRecord);
+			// 最高記録を更新
+			bestRecord = len;
+			// Player Prefsに値を保存
+			PlayerPrefs.SetFloat(key, bestRecord);
+			// 最高記録を表示
+			this.bestRecordText.GetComponent<Text>().text = "Best Record: " + bestRecord.ToString("F2") + "m";
+		}
+
+		// ゲームオーバー時
 		if (this.isGameOver) {
+			// 画面をクリックしたら
 			if (Input.GetMouseButtonDown(0)) {
 				// GameSceneを読み込む
 				SceneManager.LoadScene("GameScene");
